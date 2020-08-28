106b19db9ffdade1a826f484c04b1e7f950a3e47
fernandoreyesl <fernando.rre.ll@gmail.com>
2020-08-27 23:24:43 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -8,6 +8,16 @@ MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c749498
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 
+$preference->payment_methods = array(
+    "excluded_payment_methods" => array(
+      array("id" => "master")
+    ),
+    "excluded_payment_types" => array(
+      array("id" => "atm")
+    ),
+    "installments" => 6
+  );
+
 // Crea un ítem en la preferencia
 $item = new MercadoPago\Item();
 $item->title = 'Mi producto';
@@ -15,6 +25,7 @@ $item->quantity = 1;
 $item->unit_price = 75.56;
 $preference->items = array($item);
 $preference->save();
+
 ?>
 
 <!DOCTYPE html>
